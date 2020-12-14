f4317748e287ff45c4e05529f279fcd2eb132627
LorenzTroll <tetrix.t@googlemail.com>
2020-12-14 13:02:29 +0100

    repo with arab to roman converter. The file structure includes index.html file, a javascript file with all the logic and a simple style.css file.

---
+++ scripts/converter.js
@@ -0,0 +1,54 @@
+const inputField = document.getElementById('inputField')
+const outputField = document.getElementById('outputField');
+
+inputField.addEventListener("input", (event) => {
+  outputField.innerHTML = convertToRoman(event.target.value);
+});
+
+
+
+
+function convertToRoman(input) {
+
+  // empty string for result
+  let result = '';
+
+  // Arabic and Roman numeral characters
+  const arabicNumbers = [1000, 500, 100, 50, 10, 5, 1],
+        romanNumbers = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+
+  /* Maping arabic numbers with index. Looping over the new array while
+  the input is greater then arabic number.*/
+  arabicNumbers.map((arabic, i) => {
+     while (input >= arabic) {
+
+       /* If the input matches a condition we substract the input with
+       the subtractive notation or the numbers from the arabic array.
+       With index from .map() we identify The Roman numeral and add it
+       to result variable.*/
+       if (input == 4) {
+         input -= 4;
+         result += 'IV';
+       } else if (input == 9) {
+          input -= 9;
+          result += 'IX';
+       } else if (input >= 40 && input <= 50) {
+          input -= 40;
+          result += 'XL';
+       } else if (input >= 90 && input <= 100) {
+          input -= 90;
+          result += 'XC';
+       } else if (input >= 400 && input <= 500) {
+          input -= 400;
+          result += 'CD'
+       } else if ( input >= 900 && input <= 1000) {
+          input -= 900;
+          result += 'CM';
+       } else {
+       input -= arabic;
+       result += romanNumbers[i];
+       }
+     }
+    });
+  return result;
+}
